0293256ad90b8cdeec50b2ecab1c9c0270c51835
Aiden <apalecz2@uwo.ca>
2025-03-26 21:46:43 -0400

    Added tailwind and a main menu

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,6 @@
 import { useState } from 'react'
 import styled from 'styled-components';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import './App.css';
 import MainMenu from './components/MainMenu';
 
 
