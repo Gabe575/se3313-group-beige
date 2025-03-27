f19c5169f12dbdd9d66b6c619638134fb633eabb
Aiden <apalecz2@uwo.ca>
2025-03-26 20:19:59 -0400

    Added react app client and started setting up routes

---
+++ client/src/components/MainMenu.jsx
@@ -0,0 +1,16 @@
+
+
+
+export default function MainMenu () {
+    
+    return (
+        
+        <div>
+            <h2>Enter your name</h2>
+        </div>
+        
+        
+    )
+    
+    
+}
